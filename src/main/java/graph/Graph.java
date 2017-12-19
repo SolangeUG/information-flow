fa2c5e864dd1adfdddd5d474e27279e4c5febc0d
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2017-12-20 00:22:28 +0100

    Add method for choosing a vertex at random, and method stub for running simulations

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 public class Graph  extends SingleGraph {
 
     private HashMap<Integer, Vertex> vertices;
+    private Vertex seededVertex;
 
     /**
      * Create new empty graph
@@ -44,6 +45,57 @@ public class Graph  extends SingleGraph {
             Edge edge = new Edge(from, to);
             start.addEdge(edge);
         }
+    }
+
+    /**
+     * Return a randomly chosen seeded vertex id
+     * for simulations purposes
+     * @return a randomly choosen vertex id
+     */
+    public String getSeededVertex() {
+        seededVertex = getRandomVertex();
+        return seededVertex.getId();
+    }
+
+    /**
+     * Behavior cascade simulation algorithm
+     * @param rewardA value of incentive a
+     * @param rewardB value of incentive b
+     */
+    public void runSimulations(int rewardA, int rewardB) {
+        /*
+         * Example to help formalize our model
+         * ***********************************
+         * For two vertices v and w that are neighbors:
+         *  → if v and w both use the Netbeans IDE, they get reward a
+         *  → if v and w both use the Eclipse IDE, they get reward b
+         *  → if v and w use different IDEs, they get no reward
+         *
+         * So, the question becomes: when should a vertex switch IDEs?
+         *  →       p : the fraction of v's neighbors that use Netbeans
+         *  → (1 - p) : the fraction of v's neighbors that use Eclipse
+         *  → v should switch to Netbeans if p > [b / (a + b)]
+         * */
+
+        double threshold = ((double) rewardB) / ((double) (rewardA + rewardB));
+
+        // TODO : implement simulation algorithm
+
+
+    }
+
+    /**
+     * Return a randomly chosen seeded vertex
+     * for simulations purposes
+     * @return a randomly chosen vertex
+     */
+    private Vertex getRandomVertex() {
+        int randomIndex = (int)(Math.random() * vertices.size());
+        Vertex random = null;
+        while (random == null) {
+            random = vertices.get(randomIndex);
+        }
+        return random;
     }
 
 }
